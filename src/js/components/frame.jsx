40e6e2600461ecd64831611aecee42873fcb5001
Sujoy Chatterjee <sujoy@sumologic.com>
2019-06-06 15:08:31 +0530

    changed frame ref implementation

--- src/js/components/frame.jsx
+++ src/js/components/frame.jsx
@@ -4,26 +4,28 @@ import styles from '../../css/frame.css';
 export class Frame extends React.Component {
   constructor() {
     super();
+    this.frameRef = React.createRef();
     this.onIframeLoad = this.onIframeLoad.bind(this);
   }
 
   componentDidMount() {
     if (this.props.setRoot) {
-      this.props.setRoot(this.iframeRoot);
+      this.props.setRoot(this.frameRef.current);
     }
-    this.node.addEventListener('load', this.onIframeLoad);
+    this.frameRef.current.addEventListener('load', this.onIframeLoad);
   }
 
   onIframeLoad() {
-    this.iframeHead = this.node.contentDocument.head;
-    this.iframeRoot = this.node.contentDocument.body;
+    const node = this.frameRef.current;
+    this.iframeHead = node.contentDocument.head;
+    this.iframeRoot = node.contentDocument.body;
     this.forceUpdate();
   }
 
   render() {
     const { children, head } = this.props
     return (
-      <iframe ref={node => (this.node = node)} className="iframe-frame" src={this.props.htmlLink}>
+      <iframe ref={this.frameRef} className="iframe-frame" src={this.props.htmlLink}>
         {this.iframeHead && ReactDOM.createPortal(head, this.iframeHead)}
         {this.iframeRoot && ReactDOM.createPortal(children, this.iframeRoot)}
       </iframe>
